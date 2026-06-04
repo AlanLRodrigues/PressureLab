9ae5202219734a15094db6d080e32c40fa6b942e
Alan Rodrigues <alanrodrigues70@gmail.com>
2026-06-04 13:54:54 -0300

    Expansion of the Chemical Module

--- main.py
+++ main.py
@@ -4,7 +4,11 @@ import math
 # 1. Constantes:
 massa_molar_acido_acetico = 60.052
 massa_molar_bicarbonato = 84.007
+
+massa_molar_acetato = 82.034
 massa_molar_gas_carbonico = 44.009
+massa_molar_agua = 18.015
+
 R = 0.08206
 pressao_atmosferica = 101.3
 densidade_agua = 1000
@@ -13,7 +17,7 @@ densidade_agua = 1000
 print()
 print("Bem vindo ao PressureLab, aqui você poderá estimar a pressão do seu foguete!")
 print("A equação da reação é: CH3COOH + NaHCO3 → CH3COONa + H2O + CO2")
-print("A proporção é: 1 mol ácido = 1 mol bicarbonato")
+print("A proporção é: 1 mol de reagente = 1 mol de produto")
 print()
 
 # 3. Entrada de dados
@@ -39,20 +43,36 @@ mols_bicarbonato = massa_bicarbonato_puro / massa_molar_bicarbonato
 
 # 5. Cálculo do reagente limitante:
 if mols_acido_acetico < mols_bicarbonato:
-    mols_co2 = mols_acido_acetico
     reagente_limitante = "Ácido acético"
     reagente_excesso = "Bicarbonato"
-    excesso = massa_bicarbonato_puro - (mols_co2 * massa_molar_bicarbonato)
+
+    mols_acetato = mols_acido_acetico
+    mols_co2 = mols_acido_acetico
+    mols_h2o = mols_acido_acetico
+
+    excesso = massa_bicarbonato_puro - (mols_acetato * massa_molar_bicarbonato)
+    consumido = massa_bicarbonato_puro - excesso
 else:
-    mols_co2 = mols_bicarbonato
     reagente_limitante = "Bicarbonato"
     reagente_excesso = "Ácido acético"
-    excesso = massa_acido_acetico - (mols_co2 * massa_molar_acido_acetico)
 
-# 6. Produção de gás carbônico:
+    mols_acetato = mols_bicarbonato
+    mols_co2 = mols_bicarbonato
+    mols_h2o = mols_bicarbonato
+
+    excesso = massa_acido_acetico - (mols_acetato * massa_molar_acido_acetico)
+    consumido = massa_acido_acetico - excesso
+
+# 6. Produção de acetato de sódio:
+massa_acetato = mols_acetato * massa_molar_acetato
+
+# 7. Produção de gás carbônico:
 massa_co2 = mols_co2 * massa_molar_gas_carbonico
 
-# 7. Lei dos gases ideais:
+# 8. Produção de água:
+massa_h2o = mols_h2o * massa_molar_agua
+
+# 9. Lei dos gases ideais:
 temperatura_kelvin = temperatura_celsius + 273.15
 pressao_atm = (mols_co2 * R * temperatura_kelvin) / volume_garrafa
 pressao_kpa = pressao_atm * 101.325
@@ -67,26 +87,26 @@ elif pressao_psi <= 150:
 else:
     aviso = "Pressão muito alta. Verifique cuidadosamente a resistência do sistema."
 
-# 8. Diferença de pressão:
+# 10. Diferença de pressão:
 diferenca_pressao_kpa = pressao_kpa - pressao_atmosferica
 delta_p = diferenca_pressao_kpa * 1000
 
-# 9. Velocidade da água:
+# 11. Velocidade da água:
 velocidade_agua = math.sqrt((2*delta_p)/densidade_agua)
 velocidade_agua_kmh = velocidade_agua * 3.6
 
-# 10. Área da tubeira:
+# 12. Área da tubeira:
 area_tubeira_milimetros  = math.pi * ((diametro_tubeira / 2) ** 2)
 area_tubeira_metros = area_tubeira_milimetros / 1000000
 
-# 11. Vazão mássica:
+# 13. Vazão mássica:
 vazao_massica = densidade_agua * area_tubeira_metros * velocidade_agua
 
-# 12. Empuxo:
+# 14. Empuxo:
 empuxo = vazao_massica * velocidade_agua
 empuxo_kgf = empuxo / 9.807
 
-# 13 . Resultados
+# 15 . Resultados
 print("Resultados Químicos:")
 print()
 
@@ -96,11 +116,25 @@ print()
 
 print(f"Reagente limitante: {reagente_limitante}")
 print(f"Reagente em excesso: {reagente_excesso}")
-print(f"Massa de reagente em excesso: {excesso:.3f}")
 print()
 
-print(f"Massa de CO₂ produzido: {massa_co2:.3f} gramas")
-print(f"Mols de CO₂ produzido: {mols_co2:.3f} mols")
+print(f"Massa de {reagente_excesso} remanescente: {excesso:.3f}")
+print(f"Massa de {reagente_excesso} consumida: {consumido:.3f}")
+print()
+
+print(f"Quantidade de acetato de sódio produzido:")
+print(f"• g: {massa_acetato:.3f} gramas")
+print(f"• mol: {mols_acetato:.3f} mols")
+print()
+
+print("Quantidade de CO₂ produzido:")
+print(f"• g: {massa_co2:.3f} gramas")
+print(f"• mol: {mols_co2:.3f} mols")
+print()
+
+print("Quantidade de H₂O produzido:")
+print(f"• g: {massa_h2o:.3f} gramas")
+print(f"• mol: {mols_h2o:.3f} mols")
 print()
 
 print("Resultados de Pressão:")
